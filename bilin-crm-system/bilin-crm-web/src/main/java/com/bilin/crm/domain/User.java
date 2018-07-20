5ac7f7f0d542db8f940546d721ee406d883679fc
bilinma <ma_xiaobin612@126.com>
2018-07-20 16:19:23 +0800

    modify

--- bilin-crm-system/bilin-crm-web/src/main/java/com/bilin/crm/domain/User.java
+++ bilin-crm-system/bilin-crm-web/src/main/java/com/bilin/crm/domain/User.java
@@ -7,12 +7,16 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Data;
+
 /**
  * t_user
  * @author 
  */
 @Entity
 @Table(name = "t_user")
+
+@Data
 public class User implements Serializable {
     /**
 	 * 
@@ -60,67 +64,4 @@ public class User implements Serializable {
      */
     private Date createTime;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUserCode() {
-        return userCode;
-    }
-
-    public void setUserCode(String userCode) {
-        this.userCode = userCode;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-    
-    public String getTelephone() {
-		return telephone;
-	}
-
-	public void setTelephone(String telephone) {
-		this.telephone = telephone;
-	}
-
-	public String getWechat() {
-		return wechat;
-	}
-
-	public void setWechat(String wechat) {
-		this.wechat = wechat;
-	}
-
-	public String getRoleCode() {
-		return roleCode;
-	}
-
-	public void setRoleCode(String roleCode) {
-		this.roleCode = roleCode;
-	}
-
-	public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
 }
